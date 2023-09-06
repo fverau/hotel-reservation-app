76f766104c3a4694f64e6921162e24af61d6bc5d
Fabian Vera <fvera.dev@gmail.com>
2023-09-06 11:56:06 -0300

    add BookingForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Switch } from "wouter";
+import { Toaster } from "react-hot-toast";
 import HotelList from "./components/HotelList";
 import HotelDetail from "./components/HotelDetail";
 
@@ -7,12 +8,15 @@ const client = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={client}>
-      <Switch>
-        <Route path="/" component={HotelList} />
-        <Route path="/hotel/:id" component={HotelDetail} />
-      </Switch>
-    </QueryClientProvider>
+    <>
+      <Toaster position="top-left" reverseOrder={false} />
+      <QueryClientProvider client={client}>
+        <Switch>
+          <Route path="/" component={HotelList} />
+          <Route path="/hotel/:id" component={HotelDetail} />
+        </Switch>
+      </QueryClientProvider>
+    </>
   );
 }
 
